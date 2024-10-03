bcf78f0ddc9d1327b32c0d2f320ffbbc66fc378d
Kajal Mehta <kajalm@andrew.cmu.edu>
2024-10-03 03:16:59 -0400

    added api/oh-queue.js

---
+++ src/api/oh-queue.js
@@ -0,0 +1,18 @@
+// source chatGPT
+
+// const controllers = require.main.require('./src/controllers');
+
+'use strict';
+
+module.exports = (app, middleware) => {
+	app.get('/oh-queue', middleware.buildHeader, (req, res) => {
+		res.render('oh-queue', {});
+	});
+
+	app.get('/api/oh-queue', (req, res) => {
+		res.json({
+			message: 'Welcome to the Office Hours Queue page!',
+		});
+	});
+};
+
