e8ace64709f55c21b7d000523c8ec3c337ee5c2b
J0nathanLai <112527440+J0nathanLai@users.noreply.github.com>
2024-11-11 16:29:40 -0500

    Changed translator service API URL inside src/translate/index.js

--- src/translate/index.js
+++ src/translate/index.js
@@ -4,7 +4,7 @@ const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
     // Edit the translator URL below
-    const TRANSLATOR_API = "https://nodebb-f24-translator.azurewebsites.net/"
+    const TRANSLATOR_API = "https://https://nodebb-translator-wooshiland.azurewebsites.net/"
     const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
     const data = await response.json();
     return [data["is_english"], data["translated_content"]]
