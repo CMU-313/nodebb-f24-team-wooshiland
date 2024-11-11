66b9b108e973364bdba39ac4e5cf9b4a22b3e70d
J0nathanLai <112527440+J0nathanLai@users.noreply.github.com>
2024-11-11 16:37:17 -0500

    Fix Linter issues in src/translate/index.js

--- src/translate/index.js
+++ src/translate/index.js
@@ -3,9 +3,9 @@ var request = require('request');
 const translatorApi = module.exports;
 
 translatorApi.translate = async function (postData) {
-    // Edit the translator URL below
-    const TRANSLATOR_API = "https://https://nodebb-translator-wooshiland.azurewebsites.net/"
-    const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
-    const data = await response.json();
-    return [data["is_english"], data["translated_content"]]
+	// Edit the translator URL below
+	const TRANSLATOR_API = "https://nodebb-translator-wooshiland.azurewebsites.net/"
+	const response = await fetch(TRANSLATOR_API+'/?content='+postData.content);
+	const data = await response.json();
+	return [data["is_english"], data["translated_content"]]
 }
